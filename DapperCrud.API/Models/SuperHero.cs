5d2335ebed3b2fc92fcee39b60b88ba0818fb170
Keith Stanley <iamstan13y@gmail.com>
2023-09-18 12:17:52 +0200

    WIP #1.

---
+++ DapperCrud.API/Models/SuperHero.cs
@@ -0,0 +1,10 @@
+﻿namespace DapperCrud.API.Models;
+
+public class SuperHero
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set;}
+    public string? Place { get; set; }
+}
