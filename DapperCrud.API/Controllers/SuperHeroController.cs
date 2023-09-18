eeeed0262c2633a69abb71ad08e7ffa84c490979
Keith Stanley <iamstan13y@gmail.com>
2023-09-18 12:17:48 +0200

    WIP #1.

---
+++ DapperCrud.API/Controllers/SuperHeroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DapperCrud.API.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class SuperHeroController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public SuperHeroController(IConfiguration configuration) => _configuration = configuration;
+}
